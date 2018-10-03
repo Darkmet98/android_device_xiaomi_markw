4ba689357294c0ca6bbb694e30ea5065e1a744fe
Darkmet <iinfernape1998@outlook.es>
2018-10-03 09:06:18 +0200

    dotOS Bring Up

--- dot.mk
+++ dot.mk
@@ -16,10 +16,10 @@
 
 $(call inherit-product, device/xiaomi/markw/full_markw.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common dotOS stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
 
-PRODUCT_NAME := lineage_markw
+PRODUCT_NAME := dot_markw
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
